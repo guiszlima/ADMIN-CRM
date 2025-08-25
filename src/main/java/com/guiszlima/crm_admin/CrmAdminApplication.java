9f18018be2eb833e33486e372037fec95c675f1d
guiszlima <gui.spicacci.dev@gmail.com>
2025-08-24 23:22:48 -0300

    first commit

---
+++ src/main/java/com/guiszlima/crm_admin/CrmAdminApplication.java
@@ -0,0 +1,13 @@
+package com.guiszlima.crm_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmAdminApplication.class, args);
+	}
+
+}
